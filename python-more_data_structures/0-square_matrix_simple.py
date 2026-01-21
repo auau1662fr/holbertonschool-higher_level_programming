5f455d37f2a1f6a32c5a0415d8e360eb1be15203
auau1662fr <auric.chen.2005@gmail.com>
2026-01-21 14:21:13 +0100

    Python - More Data Structures add Tasks 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-
-    if len(matrix) > 0:
-        for elems in matrix[:]:
-            new_matrix.append(list(map(lambda x: x ** 2, elems)))
-
-    return new_matrix
+    return [[value * value for value in row] for row in matrix]
