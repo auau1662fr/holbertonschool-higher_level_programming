f15128bac178d20632df4e9b4e09f300a247ca21
auau1662fr <auric.chen.2005@gmail>
2026-01-22 15:03:50 +0100

    add 2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,8 +6,8 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
             count += 1
         except (TypeError, ValueError):
-            pass
-        except IndexError:  # quand x > len(my_list)
+            continue
+        except IndexError:
             break
     print()
     return count
