5dbd8ebd65b2364448b6def10897cf983429cc3d
auau1662fr <auric.chen.2005@gmail>
2026-01-22 14:22:44 +0100

    Python exceptions: Implement tasks 0-6 with proper error handling

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    i = 0
+    try:
+        while i < x:
+            try:
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
+            except (TypeError, ValueError):
+                pass
+            i += 1
+        print()
+    except IndexError:
+        print()
+    return count
+
