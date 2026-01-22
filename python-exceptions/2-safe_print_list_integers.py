4b818f10f3946e2c6b118bfb5b09c850779dec6d
auau1662fr <auric.chen.2005@gmail>
2026-01-22 14:51:37 +0100

    Python exceptions: Implement tasks 0-6 with proper error handling

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -14,4 +14,3 @@ def safe_print_list_integers(my_list=[], x=0):
     except IndexError:
         print()
     return count
-
--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def raise_exception_msg(message=""):
-    raise NameError(message)#!/usr/bin/python3
+    raise NameError(message)  # raise an exception
