86412e08872cb958b2d202965d10ba4b76a28538
auau1662fr <auric.chen.2005@gmail.com>
2026-02-04 11:34:02 +0100

    python-inheritance 0 1 2 3

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -7,18 +7,8 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """
-        Validates that value is a positive integer.
-
-        Args:
-            name (str): name of the parameter
-            value: value to validate
-
-        Raises:
-            TypeError: if value is not an integer
-            ValueError: if value <= 0
-        """
+        """Validates that value is a positive integer."""
         if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
