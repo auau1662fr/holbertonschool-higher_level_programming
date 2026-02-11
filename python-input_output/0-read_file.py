510c21a76134e8e81cecc1803adc8a0f1241a572
auau1662fr <auric.chen.2005@gmail.com>
2026-02-11 11:17:26 +0100

    python-input_output 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""Module to read a file and print its content."""
+
+
 def read_file(filename=""):
-    """Reads a text file (UTF-8) and prints it to stdout."""
+    """Reads a text file (UTF8) and prints it to stdout."""
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
