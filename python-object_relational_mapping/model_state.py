6eb86c491268336ebea6b1dd8facc1fa8ed572a2
auau1662fr <auric.chen.2005@gmail>
2026-03-05 14:50:52 +0100

    add 7 model_state.py

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""defines the State class"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State class that links to the states table"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True,
+                nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
