481131b5a18ca606edf7047b817b6ef0276e8e7f
auau1662fr <auric.chrn.2005@gmail.com>
2026-03-26 20:17:10 +0100

    add Tasks3

--- python-server_side_rendering/task_03_files.py
+++ python-server_side_rendering/task_03_files.py
@@ -22,5 +22,28 @@ def read_csv():
             })
     return products
 
+@app.route('/products')
+def products():
+    source = request.args.get('source')
+    product_id = request.args.get('id')
+
+    # Vérifier la source
+    if source == "json":
+        data = read_json()
+    elif source == "csv":
+        data = read_csv()
+    else:
+        return render_template("product_display.html", error="Wrong source")
+
+    # Filtrer par id si présent
+    if product_id:
+        product_id = int(product_id)
+        data = [p for p in data if p["id"] == product_id]
+
+        if not data:
+            return render_template("product_display.html", error="Product not found")
+
+    return render_template("product_display.html", products=data)
+
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
