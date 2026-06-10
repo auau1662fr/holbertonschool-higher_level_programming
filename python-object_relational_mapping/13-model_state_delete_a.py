5b95fd180908fa96ac3bac34c02cf42a1276d4a7
auau1662fr <auric.chrn.2005@gmail.com>
2026-06-10 13:36:20 +0200

    add 8 13

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Deletes all State objects containing 'a' in their name."""
+"""Delete State objects containing the letter a."""
 
 import sys
 from sqlalchemy import create_engine
@@ -10,17 +10,14 @@ from model_state import Base, State
 if __name__ == "__main__":
     engine = create_engine(
         "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            sys.argv[1],
-            sys.argv[2],
-            sys.argv[3]
-        ),
-        pool_pre_ping=True
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        )
     )
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).filter(State.name.like('%a%')).all()
+    states = session.query(State).filter(State.name.like('%a%'))
 
     for state in states:
         session.delete(state)
--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -10,11 +10,8 @@ from model_state import Base, State
 if __name__ == "__main__":
     engine = create_engine(
         "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            sys.argv[1],
-            sys.argv[2],
-            sys.argv[3]
-        ),
-        pool_pre_ping=True
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        )
     )
 
     Session = sessionmaker(bind=engine)
@@ -22,9 +19,9 @@ if __name__ == "__main__":
 
     state = session.query(State).order_by(State.id).first()
 
-    if state is not None:
-        print("{}: {}".format(state.id, state.name))
-    else:
+    if state is None:
         print("Nothing")
+    else:
+        print("{}: {}".format(state.id, state.name))
 
     session.close()
