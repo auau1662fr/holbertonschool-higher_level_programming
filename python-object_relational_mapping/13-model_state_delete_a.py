7d468b03466594b2cd97f3e1d2d957cf1cad198e
auau1662fr <auric.chrn.2005@gmail.com>
2026-06-10 14:15:25 +0200

    add 8 13

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python3
-"""Delete State objects containing the letter a."""
-
+#!/usr/bin/python3
+"""Deletes all State objects with a name containing the letter a"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -8,19 +7,11 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        )
-    )
-
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    states = session.query(State).filter(State.name.like('%a%'))
-
-    for state in states:
-        session.delete(state)
-
+    session.query(State).filter(State.name.like('%a%')).delete(
+        synchronize_session=False)
     session.commit()
     session.close()
--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python3
-"""Prints the first State object from the database."""
-
+#!/usr/bin/python3
+"""Prints the first State object from the database"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -8,20 +7,13 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        )
-    )
-
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
-
     state = session.query(State).order_by(State.id).first()
-
     if state is None:
         print("Nothing")
     else:
         print("{}: {}".format(state.id, state.name))
-
     session.close()
